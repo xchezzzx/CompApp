f6e0c93b66f18d0298ac8c7870ba84cda72c5572
veregant <veregant@gmail.com>
2023-01-04 20:46:20 +0200

    Add SignalR test hub

---
+++ backend/ASP.NETCore/Hubs/TestHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ASP.NETCore.Hubs
+{
+	public class TestHub : Hub
+	{
+		public async Task SendTestData()
+		{
+			string testMessage = "Test data is delivered";
+
+			await Clients.Caller.SendAsync("GetTestData", testMessage);
+		}
+	}
+}
--- backend/ASP.NETCore/Program.cs
+++ backend/ASP.NETCore/Program.cs
@@ -1,3 +1,5 @@
+using ASP.NETCore.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -19,6 +22,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapHub<TestHub>("/test");
 
 app.MapControllers();
 
