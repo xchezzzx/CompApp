ce432fcc3d21804b9b791a04efe4715dffc8bc88
xchezzzx <50829021+xchezzzx@users.noreply.github.com>
2023-01-07 00:36:30 +0200

    Adding doesn't work

--- frontend/BlazorWeb/Models/DataTransferModels/CompetitionDT.cs
+++ frontend/BlazorWeb/Models/DataTransferModels/CompetitionDT.cs
@@ -3,7 +3,7 @@
     public class CompetitionDT
     {
 
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string CompetitionName { get; set; } = null!;
         public TimeSpan Duration { get; set; }
         public DateTime StartTime { get; set; }
