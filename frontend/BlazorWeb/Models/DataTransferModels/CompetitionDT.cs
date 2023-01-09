4ff58405fd331a087f6bbfb2e6df68c40959c016
xchezzzx <50829021+xchezzzx@users.noreply.github.com>
2023-01-10 00:33:52 +0200

    I want to com mit

--- frontend/BlazorWeb/Models/DataTransferModels/CompetitionDT.cs
+++ frontend/BlazorWeb/Models/DataTransferModels/CompetitionDT.cs
@@ -1,19 +1,15 @@
 ﻿namespace BlazorWeb.Models.DataTransferModels
 {
-    public class CompetitionDT
-    {
-
-        public int? Id { get; set; }
-        public string CompetitionName { get; set; } = null!;
-        public TimeSpan Duration { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public int NumberConcTasks { get; set; }
-        public string Hashtag { get; set; } = null!;
-
-        public CompetitionDT()
-        {
-        }
-
-    }
+	public class CompetitionDT
+	{
+		public int? Id { get; set; }
+		public string Name { get; set; } = null!;
+		public TimeSpan Duration { get; set; }
+		public DateTime StartDateTime { get; set; }
+		public DateTime EndDateTime { get; set; }
+		public int NumberConcTasks { get; set; }
+		public string Hashtag { get; set; } = null!;
+		public string State { get; set; }
+		public string Status { get; set; }
+	}
 }
